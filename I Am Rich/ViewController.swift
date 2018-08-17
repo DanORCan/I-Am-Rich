b14b138da8dc2e4fb728482917a635aaaa97d952
DANIEL OREILLY <danieloreilly@DANIELs-MBP.fios-router.home>
2018-08-16 20:00:05 -0400

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by DANIEL OREILLY on 8/16/18.
+//  Copyright © 2018 DANIEL OREILLY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
